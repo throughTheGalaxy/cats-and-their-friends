98f35922cc78cc3507c1bcc993de4594307b9b88
Nikita Ostrovskii <97059086+throughTheGalaxy@users.noreply.github.com>
2024-04-06 02:45:58 +0200

    Add files via upload

---
+++ cats-and-their-friends-not-submodule/src/App.tsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect, useContext } from "react";
+import { RouterApp } from "./router/routerapp";
+import {
+  LastLocationContext,
+  SetLastLocationContext,
+} from "./context/location";
+import { useLocation } from "react-router";
+
+function App() {
+  const [lastLocation, setLastLocation] = useState<string>("");
+  let location = useLocation();
+
+  useEffect(() => {
+    return () => {
+      setLastLocation(location.pathname.slice(1));
+    };
+  }, [location]);
+
+  return (
+    <LastLocationContext.Provider value={lastLocation}>
+      <SetLastLocationContext.Provider value={setLastLocation}>
+        <RouterApp />
+      </SetLastLocationContext.Provider>
+    </LastLocationContext.Provider>
+  );
+}
+
+export default App;
